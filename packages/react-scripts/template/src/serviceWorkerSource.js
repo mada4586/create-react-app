9cd3494b21026294d83e04999004110a143ecfc2
mada4586 <mada4586@users.noreply.github.com>
2019-01-07 16:38:13 +0100

    WorkboxWebpackPlugin - changed the way a service worker is generated, from GenerateSW to InjectManifest.

---
+++ packages/react-scripts/template/src/serviceWorkerSource.js
@@ -0,0 +1,9 @@
+workbox.clientsClaim();
+
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute('/index.html', {
+  blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/],
+});
